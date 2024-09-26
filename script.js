6620ade7a367d7ce5fc344f70f511d4e8c5e68c3
Eldan Gold <56773148+eldangold@users.noreply.github.com>
2024-09-26 21:20:30 +0200

    Define basic variables and functions for math operation

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function sum() {
+    return firstNumber + secondNumber;
+}
+
+function subtract() {
+    return firstNumber - secondNumber;
+}
+
+function multiply() {
+    return firstNumber * secondNumber;
+}
+
+function divide() {
+    return firstNumber % secondNumber;
+}
