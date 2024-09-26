eb3d8ef2faa6ce07c7419dc875b2bfa9812221e8
Eldan Gold <56773148+eldangold@users.noreply.github.com>
2024-09-27 00:23:51 +0200

    Make calculation basics work

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let firstNumber;
 let secondNumber;
 let operator;
-let displayValue;
+let displayValue = "";
 
 function sum(firstNumber, secondNumber) {
-    document.getElementById("display").textContent = firstNumber + secondNumber;
+    displayValue = Number(firstNumber) + Number(secondNumber);
+    document.getElementById("display").textContent = displayValue;
     
 }
 
@@ -21,19 +22,40 @@ function divide(firstNumber, secondNumber) {
 }
 
 function operate(firstNumber, secondNumber, operator) {
-    if (operator === "sum") {
+    if (operator === "+") {
         sum(firstNumber, secondNumber)
     }
 
-    else if (operator === "subtract") {
+    else if (operator === "-") {
         subtract(firstNumber, secondNumber)
     }
 
-    else if (operator === "multiply") {
+    else if (operator === "X") {
         multiply(firstNumber, secondNumber)
     }
 
-    else if (operator === "divide") {
+    else if (operator === "/") {
         divide(firstNumber, secondNumber)
     }
 }
+
+for (number of document.querySelectorAll(".numbers")) {
+    number.addEventListener("click", (number) =>  {
+        if (!operator) { firstNumber =  number.target.textContent;
+            displayValue = firstNumber;
+            document.getElementById("display").textContent = displayValue;}
+        else { secondNumber = (number.target.textContent);
+        displayValue += secondNumber;
+        document.getElementById("display").textContent = displayValue;
+        }
+    });
+}
+
+for (number of document.querySelectorAll(".controls")) {
+    number.addEventListener("click", (number) =>  { operator = number.target.textContent
+        displayValue += operator;
+        document.getElementById("display").textContent = displayValue;
+    });
+}
+
+document.getElementById("finishCalculation").addEventListener("click", () => operate(firstNumber,secondNumber,operator));
