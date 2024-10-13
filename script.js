6b48a199e9f5e6c9c3fb2bd526d84966201bf476
Eldan Gold <56773148+eldangold@users.noreply.github.com>
2024-10-13 12:51:55 +0200

    First alpha keyboard support

--- script.js
+++ script.js
@@ -137,3 +137,57 @@ document.getElementById("backspace").addEventListener("click", () => {
         document.getElementById("display").textContent = calcData.displayValue;
     }
 })
+
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+      return;
+    }
+    const allowedNumbers = "1234567890";
+    const allowedOperators = "-+=*xX/:Enter";
+
+    if (allowedNumbers.includes(event.key)) {
+        if (calcData.firstNumber !== "" && calcData.secondNumber !== "" && event.key !== "=") {
+            operate(calcData);
+            calcData.operator = event.key;
+            calcData.firstNumber = calcData.result;
+            calcData.secondNumber = "";
+            calcData.displayValue = calcData.firstNumber + calcData.operator;
+            document.getElementById("display").textContent = calcData.displayValue;
+            calcData.result = "";
+        }
+        else if (!calcData.operator) {
+            calcData.firstNumber += event.key;
+            calcData.displayValue = calcData.firstNumber;
+            document.getElementById("display").textContent = calcData.displayValue;
+        } 
+
+        else {
+            calcData.secondNumber += event.key;
+            calcData.displayValue = calcData.firstNumber + calcData.operator + calcData.secondNumber;
+            document.getElementById("display").textContent = calcData.displayValue;
+        }
+
+    }
+
+    if (allowedOperators.includes(event.key)) {
+        
+       
+         if (calcData.firstNumber == "" && event.key == "-") {
+            calcData.firstNumber = "-" + calcData.firstNumber;
+            calcData.displayValue = calcData.firstNumber;
+            document.getElementById("display").textContent = calcData.displayValue;
+        }
+
+        else if (calcData.firstNumber == "" && event.key !== "-") {}
+        else if (calcData.firstNumber && calcData.secondNumber && calcData.operator && (event.key == "="  || event.key == "Enter")) operate(calcData)
+
+        else if (event.key !== "=" && event.key !== "Enter") {
+            calcData.operator = event.key;
+            calcData.displayValue = calcData.firstNumber + calcData.operator;
+            document.getElementById("display").textContent = calcData.displayValue;
+        }
+
+    }
+
+    event.preventDefault();
+  }, true);
